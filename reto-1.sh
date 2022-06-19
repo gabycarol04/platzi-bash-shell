7fc068f9ee2bf8262930ef6e7b78e88369c01870
Gabriela Navarro <gabycarol90@gmail.com>
2022-06-19 05:06:55 -0500

    feat: reto-1

---
+++ reto-1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Reto 1
+#Desarrollar un programa llamado utilityHost.sh, dentro de él vamos a declarar dos variables una llamada option, otra llamada result, vamos a inicializarles e imprimir sus valores.
+
+#Yo llame al script, reto-1.sh
+
+option=Matutino
+result=Nocturno
+
+echo "Mi mejor opción para el horario de la U sería el $option, pero quede en el horario $result"
